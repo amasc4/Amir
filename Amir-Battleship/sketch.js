5be012fd22bd98398cca25ac87860fbe8cc57bef
amasc4 <19amustefa@ecfs.org>
2017-07-19 13:06:04 -0400

    First Commit

---
+++ Amir-Battleship/sketch.js
@@ -0,0 +1,206 @@
+
+function setup(){
+    createCanvas(1000, 1000);
+    background(225, 225, 225);
+}
+    var clickbox1 = false
+    var clickbox2 = false
+    var clickbox3 = false
+    var clickbox4 = false
+    var clickbox5 = false
+    var clickbox6 = false
+    var clickbox7 = false
+    var clickbox8 = false
+    var clickbox9 = false
+
+    var clickbox10 = false
+    var clickbox11 = false
+    var clickbox12 = false
+    var clickbox13 = false
+    var clickbox14 = false
+    var clickbox15 = false
+    var clickbox16 = false
+    var clickbox17 = false
+    var clickbox18 = false
+
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+    var clickbox = false
+
+function draw(){
+    fill("black")
+    rect(100, 200, 2, 600)
+    rect(200, 200, 2, 600)
+    rect(300, 200, 2, 600)
+    rect(400, 200, 2, 600)
+    rect(500, 200, 2, 600)
+    rect(600, 200, 2, 600)
+    rect(700, 200, 2, 600)
+    rect(800, 200, 2, 600)
+    rect(0, 275, 900, 2)
+    rect(0, 350, 900, 2)
+    rect(0, 425, 900, 2)
+    rect(0, 500, 900, 2)
+    rect(0, 575, 900, 2)
+    rect(0, 650, 900, 2)
+    rect(0, 725, 900, 2)
+    
+    
+    stroke("blue")
+    var shipColor = "white"
+    fill(shipColor)
+    var choice1 = rect(0, 200, 100, 75)
+    var choice2 = rect(0, 275, 100, 75)
+    var choice3 = rect(0, 350, 100, 75)
+    var choice4 = rect(0, 425, 100, 75)
+    var choice5 = rect(0, 500, 100, 75)
+    var choice6 = rect(0, 575, 100, 75)
+    var choice7 = rect(0, 650, 100, 75)
+    var choice8 = rect(0, 725, 100, 75)
+    
+    var choice9 = rect(100, 200, 100, 75)
+    var choice10 = rect(100, 275, 100, 75)
+    var choice11 = rect(100, 350, 100, 75)
+    var choice12 = rect(100, 425, 100, 75)
+    var choice13 = rect(100, 500, 100, 75)
+    var choice14 = rect(100, 575, 100, 75)
+    var choice15 = rect(100, 650, 100, 75)
+    var choice16 = rect(100, 725, 100, 75)
+
+    var choice17 = rect(200, 200, 100, 75)
+    var choice18 = rect(200, 275, 100, 75)
+    var choice19 = rect(200, 350, 100, 75)
+    var choice20 = rect(200, 425, 100, 75)
+    var choice21 = rect(200, 500, 100, 75)
+    var choice22 = rect(200, 575, 100, 75)
+    var choice23 = rect(200, 650, 100, 75)
+    var choice24 = rect(200, 725, 100, 75)
+
+    var choice25 = rect(300, 200, 100, 75)
+    var choice26 = rect(300, 275, 100, 75)
+    var choice27 = rect(300, 350, 100, 75)
+    var choice28 = rect(300, 425, 100, 75)
+    var choice29 = rect(300, 500, 100, 75)
+    var choice30 = rect(300, 575, 100, 75)
+    var choice31 = rect(300, 650, 100, 75)
+    var choice32= rect(300, 725, 100, 75)
+
+    var choice33 = rect(400, 200, 100, 75)
+    var choice34 = rect(400, 275, 100, 75)
+    var choice35 = rect(400, 350, 100, 75)
+    var choice36 = rect(400, 425, 100, 75)
+    var choice37 = rect(400, 500, 100, 75)
+    var choice38 = rect(400, 575, 100, 75)
+    var choice39 = rect(400, 650, 100, 75)
+    var choice40 = rect(400, 725, 100, 75)
+
+    var choice41 = rect(500, 200, 100, 75)
+    var choice42 = rect(500, 275, 100, 75)
+    var choice43 = rect(500, 350, 100, 75)
+    var choice44 = rect(500, 425, 100, 75)
+    var choice45 = rect(500, 500, 100, 75)
+    var choice46 = rect(500, 575, 100, 75)
+    var choice47 = rect(500, 650, 100, 75)
+    var choice48 = rect(500, 725, 100, 75)
+
+    var choice49 = rect(600, 200, 100, 75)
+    var choice50 = rect(600, 275, 100, 75)
+    var choice51 = rect(600, 350, 100, 75)
+    var choice52 = rect(600, 425, 100, 75)
+    var choice53 = rect(600, 500, 100, 75)
+    var choice54 = rect(600, 575, 100, 75)
+    var choice55 = rect(600, 650, 100, 75)
+    var choice56 = rect(600, 725, 100, 75)
+
+    var choice57 = rect(700, 200, 100, 75)
+    var choice58 = rect(700, 275, 100, 75)
+    var choice59 = rect(700, 350, 100, 75)
+    var choice60 = rect(700, 425, 100, 75)
+    var choice61 = rect(700, 500, 100, 75)
+    var choice62 = rect(700, 575, 100, 75)
+    var choice63 = rect(700, 650, 100, 75)
+    var choice64 = rect(700, 725, 100, 75)
+
+    var choice65 = rect(800, 200, 100, 75)
+    var choice66 = rect(800, 275, 100, 75)
+    var choice67 = rect(800, 350, 100, 75)
+    var choice68 = rect(800, 425, 100, 75)
+    var choice69 = rect(800, 500, 100, 75)
+    var choice70 = rect(800, 575, 100, 75)
+    var choice71 = rect(800, 650, 100, 75)
+    var choice72 = rect(800, 725, 100, 75)
+
+    if (mouseIsPressed) {
+        if (mouseX < 100 && mouseY > 200 && mouseY < 275){
+            clickbox1 = true
+
+        }if (mouseX < 100 && mouseY > 275 && mouseY < 350)
+            clickbox2 = true
+    print(mouseIsPressed);
+}
+    if (clickbox1 == true) {
+        fill("blue")
+        rect(0, 200, 100, 75)
+}   if (clickbox2 == true) {
+        fill("blue")
+        rect(0, 275, 100, 75)
+}
+
+}
+
--- Amir-pong/sketch.js
+++ Amir-pong/sketch.js
@@ -1,19 +1,18 @@
-var x = 100
-var y = 100
+var x = 250
+var y = 250
 var a = 480
 var b = 100
 var c = 0
 var d = 100
-var xregulate = 5
-var yregulate = 5
+var xregulate = 10
+var yregulate = 15
 
 function setup(){
     createCanvas(500, 500)
-    background(225)
 }
 
 function draw(){
-    background(225);
+    background(100, 120, 14);
     if (keyIsDown(DOWN_ARROW)){
         b+=10
         if (b>495) {
@@ -45,21 +44,37 @@ function draw(){
         }
 
     }
-
-    
     clear();
-    fill(250, 0, 0)
+    fill(225, 12, 220)
     rect(a, b, 19, 130)
     rect(c, d, 19, 130)
-    fill(250, 0, 0)
+    fill(225, 0, 0)
     ellipse(x, y, 15, 15);
     x = xregulate + x
-    if (x == a && b-130 < y < b) {
+    if (x == 508) {
+        textSize(40)
+        text("Game Over", 250, 250)
+        fill(50, 50, 50)
+    }
+    if (x == 480 && (b+30) < y && y < (b+100)) {
+        xregulate = -xregulate
+    }else if (x == 480 && b < y && y <= (b+30)){
+        xregulate = -xregulate
+        x = xregulate + x
+        y = yregulate + y
+    }else if (x == 480 && (b+100) <= y && y < (b+130)){
+        xregulate = -xregulate
+        yregulate = -yregulate
+    }
+    if (x == 20 && d < y && y < (d+130)) {
         xregulate = -xregulate
     }
-    if (x == c && d-130 < y < d)
-        xregulate = -xregulate
-    y = yregulate + y
-
-    
+    if (x == 500 || x == 0) {
+        window.alert("Game Over")
+    }
+    // y = yregulate + y
+    // if (y > 425 || y < 50){
+    //     yregulate = -yregulate
+    // }
+ 
 }
