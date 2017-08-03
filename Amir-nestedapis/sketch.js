b22ad61061beaa93c6c269c5a527e148ac55aafe
amasc4 <19amustefa@ecfs.org>
2017-08-03 11:41:19 -0400

    First

--- Amir-nestedapis/sketch.js
+++ Amir-nestedapis/sketch.js
@@ -7,14 +7,6 @@ $.ajax({
         $("body").append("<h1> Name: " + info.name.first + " " + info.name.last + "<br /> Location: " + info.location.city +" <br /> Email: " +  info.email + "</h1>")
     }
 })
-var x = info.nat
-console.log(info.nat)
-$.ajax({
-    url: "https://restcountries.eu/rest/v2/alpha?codes="+ +"",
-    dataType: 'json',
-    success: function(data) {
-        var country = data
-        
-    }
-})
+
+
 
